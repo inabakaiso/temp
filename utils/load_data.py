d316257baeb0a7e6ec28cc6a43d63aa98bc1ecad
稲葉　海 <76681344+inabakaiso@users.noreply.github.com>
2021-05-23 18:06:47 +0900

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def load_data(path):
-    train = pd.read_csv(path + '/train_csv')
-    test = pd.read_csv(path + '/test_csv')
+    train = pd.read_csv(path + '/train.csv')
+    test = pd.read_csv(path + '/test.csv')
+    return train, test
     
 def train_load_data():
     df_train = pd.read_csv(train_path)
