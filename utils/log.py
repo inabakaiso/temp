b2c5aae1cb06c855e79edd52046d4a8c255f778b
kaiso inaba <76681344+inabakaiso@users.noreply.github.com>
2021-10-10 17:23:56 +0900

    Update log.py

--- utils/log.py
+++ utils/log.py
@@ -5,6 +5,22 @@ DIR = 'the Path to this dir'
 
 logger = getLogger(__name__)
 
+#関数
+def logger():
+    log_fmt = Formatter('%(asctime)s %(name)s %(linear)d [%(levelname)s][%(funcName)s][%(message)s ')
+    handler = StreamHandler()
+    handler.setlevel('INFO')
+    hendler.setFormatter(log_fmt)
+    logger.addHandler(handler)
+    
+    handler = FileHandler(DIR + 'train.py.log', 'a')
+    handler.setlevel(DEBUG)
+    handler.setFormatter(log_fmt)
+    logger.setlevel(DEBUG)
+    logger.addHandler(handler)
+    
+    return logger
+
 if __name__ == '__main__':
     log_fmt = Formatter('%(asctime)s %(name)s %(linear)d [%(levelname)s][%(funcName)s][%(message)s ')
     handler = StreamHandler()
@@ -20,21 +36,4 @@ if __name__ == '__main__':
     #以下から通常の文を書き進めつつ
     #logger.info('注意事項')
     #logger.debug('')
-
-
-#関数
-def logger():
-        log_fmt = Formatter('%(asctime)s %(name)s %(linear)d [%(levelname)s][%(funcName)s][%(message)s ')
-    handler = StreamHandler()
-    handler.setlevel('INFO')
-    hendler.setFormatter(log_fmt)
-    logger.addHandler(handler)
-    
-    handler = FileHandler(DIR + 'train.py.log', 'a')
-    handler.setlevel(DEBUG)
-    handler.setFormatter(log_fmt)
-    logger.setlevel(DEBUG)
-    logger.addHandler(handler)
-    
-    return logger
 
